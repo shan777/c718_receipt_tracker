e74311e3f8b2affba9e5235a683878e66dc755ed
nebevets <steveben@exmsft.com>
2018-10-01 13:02:52 -0700

    Redoing mern stack environment setup

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import Test from './test';
 
 const App = () => (
     <div>
         <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
+            <Test/>
         </div>
     </div>
 );
