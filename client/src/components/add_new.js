6c904725c2b6bc2e98a847e2f1025ac23b8d3699
Sarah Han <sarah.s.han@gmail.com>
2018-10-02 20:39:27 -0700

    addNew inputs added

--- client/src/components/add_new.js
+++ client/src/components/add_new.js
@@ -18,6 +18,8 @@ class AddNew extends Component {
         return (
             <div>
                 <form onSubmit={this.handleAddItem}>
+                <button type="reset" value="X">X</button>
+                <button type="submit" value="Done">Done</button>
                     <div className="row">
                         <div className="col s8 offset-s2">
                             <label>Merchant Name:</label>
@@ -27,6 +29,7 @@ class AddNew extends Component {
                             />
                         </div>
                     </div>
+
                     <div className="row">
                         <div className="col s8 offset-s2">
                             <label>Date of Purchase:</label>
@@ -37,6 +40,46 @@ class AddNew extends Component {
                             />
                         </div>
                     </div>
+
+                    <div className="row">
+                        <div className="col s8 offset-s2">
+                            <label>Total Amount:</label>
+                            <input onChange={ (e) => this.setState({totalAmount: e.target.value})}
+                                type="number"
+                                value={totalAmount}
+                            />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col s8 offset-s2">
+                            <label>Category:</label>
+                            <input onChange={ (e) => this.setState({category: e.target.value})}
+                                type="text"
+                                value={category}
+                            />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col s8 offset-s2">
+                            <label>Note:</label>
+                            <input onChange={ (e) => this.setState({note: e.target.value})}
+                                type="text"
+                                value={note}
+                            />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col s8 offset-s2">
+                            <label>Receipt Image:</label>
+                            <input onChange={ (e) => this.setState({receiptImageSrc: e.target.value})}
+                                type="text"
+                                value={receiptImageSrc}
+                            />
+                        </div>
+                    </div>
+
+
+
                 </form>
             </div>
 
