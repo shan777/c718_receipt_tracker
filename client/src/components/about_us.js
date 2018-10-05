ab050cd02d38a88d00a1dbd029e87cd1ff9e84e6
Sarah Han <sarah.s.han@gmail.com>
2018-10-05 10:17:14 -0700

    pictures updated

--- client/src/components/about_us.js
+++ client/src/components/about_us.js
@@ -16,56 +16,56 @@ class AboutUs extends Component{
         return(
             <div className="about_us_container">
                 <img className="logo_img" src={meetTeam} />
-<div className="members">
-                <div className="member">
-                    <img className="img_left" src={chase}/>Chase Caine
-                    <a href="https://www.linkedin.com/in/chase-caine-2a86b5169/">
-                    <img src={linkedin_logo} alt="linkedin logo"></img>
-                    </a>
-                    <a href="https://www.linkedin.com/in/chase-caine-2a86b5169/">
-                        <img src={github_logo} alt="github logo"></img>
-                    </a>
-                </div>
-
-               <div className="member">
-                    <img className="img_right" src={esther}/>Esther Suh
-                    <a href="https://www.linkedin.com/in/esther-suh-8b426015b/">
-                    <img  src={linkedin_logo} alt="linkedin logo"></img>
-                    </a>
-                    <a href="https://www.linkedin.com/in/esther-suh-8b426015b/">
-                        <img src={github_logo} alt="github logo"></img>
-                    </a>
-                </div>
-
-                <div className="member">
-                    <img className="img_left" src={sarah}/>Sarah Han
-                    <a href="https://www.linkedin.com/sarah-han-1120">
+                <div className="everybody">
+                    <div className="member">
+                        <img className="img_left" src={chase}/>Chase Caine
+                        <a href="https://www.linkedin.com/in/chase-caine-2a86b5169/">
                         <img src={linkedin_logo} alt="linkedin logo"></img>
-                    </a>
-                    <a href="https://www.linkedin.com/in/esther-suh-8b426015b/">
-                        <img src={github_logo} alt="github logo"></img>
-                    </a>
-                </div>
-                
-                <div className="member">
-                    <img className="img_right" src={kyle}/>Kyle Pamintuan
-                    <a href="https://www.linkedin.com/in/kylepamintuan/">
-                    <img  src={linkedin_logo} alt="linkedin logo"></img>
-                    </a>
-                    <a href="https://www.linkedin.com/in/kylepamintuan/">
-                        <img src={github_logo} alt="github logo"></img>
-                    </a>
-                </div>
+                        </a>
+                        <a href="https://www.linkedin.com/in/chase-caine-2a86b5169/">
+                            <img src={github_logo} alt="github logo"></img>
+                        </a>
+                    </div>
 
                 <div className="member">
-                    <img className="img_left" src={steve}/>Steve Benedict
-                    <a href="https://www.linkedin.com/in/nebevets/">
-                    <img src={linkedin_logo} alt="linkedin logo"></img>
-                    </a>
-                    <a href="https://www.linkedin.com/in/nebevets/">
-                        <img src={github_logo} alt="github logo"></img>
-                    </a>
-                </div>
+                        <img className="img_right" src={esther}/>Esther Suh
+                        <a href="https://www.linkedin.com/in/esther-suh-8b426015b/">
+                        <img  src={linkedin_logo} alt="linkedin logo"></img>
+                        </a>
+                        <a href="https://www.linkedin.com/in/esther-suh-8b426015b/">
+                            <img src={github_logo} alt="github logo"></img>
+                        </a>
+                    </div>
+
+                    <div className="member">
+                        <img className="img_left" src={sarah}/>Sarah Han
+                        <a href="https://www.linkedin.com/sarah-han-1120">
+                            <img src={linkedin_logo} alt="linkedin logo"></img>
+                        </a>
+                        <a href="https://www.linkedin.com/in/esther-suh-8b426015b/">
+                            <img src={github_logo} alt="github logo"></img>
+                        </a>
+                    </div>
+                    
+                    <div className="member">
+                        <img className="img_right" src={kyle}/>Kyle Pamintuan
+                        <a href="https://www.linkedin.com/in/kylepamintuan/">
+                        <img  src={linkedin_logo} alt="linkedin logo"></img>
+                        </a>
+                        <a href="https://www.linkedin.com/in/kylepamintuan/">
+                            <img src={github_logo} alt="github logo"></img>
+                        </a>
+                    </div>
+
+                    <div className="member">
+                        <img className="img_left" src={steve}/>Steve Benedict
+                        <a href="https://www.linkedin.com/in/nebevets/">
+                        <img src={linkedin_logo} alt="linkedin logo"></img>
+                        </a>
+                        <a href="https://www.linkedin.com/in/nebevets/">
+                            <img src={github_logo} alt="github logo"></img>
+                        </a>
+                    </div>
                 </div>
             </div>
         )
