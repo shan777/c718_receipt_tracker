0118cefb7d4b0ee638822882e4068523bdbbf2b6
nebevets <steveben@exmsft.com>
2018-10-06 13:35:01 -0700

    getTagsForReceipt endpoint created

--- index.js
+++ index.js
@@ -42,6 +42,41 @@ server.post('/api/addTag', (request, response) => {
     }
 });
 
+server.post('/api/getTagsForReceipt', (request, response) => {
+    const {receiptId} = request.body;
+    console.log("request data: ", request.body);
+    
+    const output = {
+        tags: [],
+        success: false
+    };
+
+    let receiptIdRegEx = /^[1-9][\d]*/;
+
+    if (receiptIdRegEx.exec(receiptId)){
+        const connection = mysql.createConnection(sqrlDbCreds);
+        connection.query("SELECT receipts_tags.tagId,tags.tagName FROM receipts_tags JOIN tags ON receipts_tags.tagId=tags.ID WHERE receipts_tags.receiptId=?",
+                    [receiptId],
+                    (error, rows) => {
+                        console.log('tags for receipt query made');
+                        if (error){
+                            console.log('tags for receipt query error', error);
+                            response.send(output);
+                        }
+                        rows.forEach(element => {
+                            output.tags.push(element);
+                        });
+                        output.success = true;
+                        connection.end(() => {
+                            console.log('connection end');
+                        });                        
+                        response.send(output);
+                    });
+    }else{
+        response.send(output);
+    }
+});
+
 server.get('*', (request, response) => {
     response.sendFile(resolve(__dirname, 'client', 'dist', 'index.html')); // resolve ensures a correct path
 });
