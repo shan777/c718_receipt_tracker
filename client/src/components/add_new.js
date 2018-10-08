a46983c827a21721b2fd2202337f72d1d8ebfc63
Sarah Han <sarah.s.han@gmail.com>
2018-10-08 00:20:22 -0700

    functionality added to addNew page

--- client/src/components/add_new.js
+++ client/src/components/add_new.js
@@ -5,7 +5,7 @@ import Header from './header';
 import Footer from './footer';
 import axios from 'axios';
 import './add_new.css';
-import Dummy from '../dummy_data/dummyList';
+import dummy from '../dummy_data/dummyList';
 
 
 
@@ -25,23 +25,54 @@ class AddNew extends Component {
         dateOfPurchase: '',
         category: '',
         note: '',
-        receiptImageSrc: ''
+        tag: true,
+    }
+
+    //newTag???????????????????????????????????????
+
+    handleSubmit = (event) => {
+        console.log('dummy: ',{dummy});
+    
+        //how do i find the user? 
+        //isn't it better 
+        event.preventDefault();
+
+        const {merchantName, dateOfPurchase, totalAmount, category, note, tag} = this.state;
+        console.log('merchantName: ', merchantName);
+
+        if(tag) {
+            console.log('tag:', tag);
+        }
+
+        this.props.history.push('/overview');
+    }
+
+    addNewTag = (event) => {
+        //add new tag to the tag array
+        event.preventDefault();
+    }
+
+    clearForm() {
+        console.log('clear form');
     }
 
     render() {
-        const {merchantName, dateOfPurchase, totalAmount, category, note, receiptImageSrc} = this.state;
+        const {merchantName, dateOfPurchase, totalAmount, category, note, tag} = this.state;
+        const categoryArray = ['Dining', 'Groceries', 'Shopping', 'Beauty', 'Health', 'Transportation', 'Lodging', 'Repairs'];
+        const categoryChoices = categoryArray.map((option) => 
+            <option key={option} value={category}>{option}</option>);
 
         return (
             <div>
                 <Header title="Add New"/>
                 <div className="main_container">
-                    <form action="#">
+                    <form action="#" onSubmit={this.handleSubmit}>
                         <div className="btn_container">
-                            <button className="cancel_btn" type="reset" value="X">X</button>
-                            <button className="done_btn"  type="submit" value="Done" onSubmit={this.handleAddItem}>Done</button>
+                            <button className="cancel_btn" type="reset" value="X" onSubmit={this.clearForm}>X</button>
+                            <button className="done_btn"  type="submit" value="Done">Done</button>
                         </div>    
                         <div className="row content_container">
-                                <label className="input_title">Merchant Name:</label>
+                                <label className="input_title">Merchant Name</label>
                                 <input className="merchName input_field" placeholder="Not specified" onChange={ (e) => this.setState({merchantName: e.target.value})}
                                     type="text"
                                     value={merchantName}
@@ -49,8 +80,7 @@ class AddNew extends Component {
                         </div>
 
                         <div className="row content_container">
-                                <label className="input_title">Date of Purchase:</label>
-                                {/* <input onChange={ (e) => this.setState({dateOfPurchase: e.target.value})} */}
+                                <label className="input_title">Date of Purchase</label>
                                 <input onChange={ (e) => this.setState({dateOfPurchase: e.target.value})}
                                     type="date"
                                     value={dateOfPurchase}
@@ -59,31 +89,35 @@ class AddNew extends Component {
 
                         <div className="row content_container">
                                 <label className="input_title">Total Amount:</label>
-                                $ <input className="merchName input_field" onChange={ (e) => this.setState({totalAmount: e.target.value})}
+                                $ <input className="merchName input_field" onChange={ (e) => this.setState({totalAmount: (e.target.value)})} 
+                                // e.target.value * 100 --> sending in pennies to server -----> GOTTA DO THIS!!!!!!!!!!
                                     type="number" min="0.00" step="0.01"
                                     value={totalAmount}
                                 />
+                        </div> 
 
-                        </div>
                         <div className="row content_container">
-                                <label className="input_title">Category:</label>
-                                <select name="category">
-                                    <option value={category}>Dining</option>
-                                    <option value={category}>Groceries</option>
-                                    <option value={category}>Beauty</option>
-                                    <option value={category}>Health</option>
+                                <label className="input_title">Category</label>
+                                <select name="category" onChange={ (e) => this.setState({category: (e.target.value)})} value={category}>
+                                    {categoryChoices}
                                 </select>
-                                {/* <input className="merchName input_field" placeholder="Not specified" onChange={ (e) => this.setState({category: e.target.value})}
-                                    type="text"
-                                    value={category}
-                                /> */}
                         </div>
+
                         <div className="row content_container">
-                                <label className="input_title">Note:</label>
+                                <label className="input_title">Note</label>
                                 <input className="noteInput input_field" placeholder="Not specified" onChange={ (e) => this.setState({note: e.target.value})}
                                     type="text"
                                     value={note}
                                 />
+                        </div>
+
+                        <div className="row content_container">
+                                <label className="input_title">Tag</label>
+                                <input className="input_field" name="None" type="checkbox" checked={this.state.tag} onChange={ (e) => this.setState({tag: e.target.value})}
+                                    type="text"
+                                    value={tag}
+                                />
+                                {/* <label>New Tag<input name="newTag" type="text" value={newTag} onChange={this.addNewTag}/></label> */}
                         </div>
                         
                     </form>
--- client/src/components/app.js
+++ client/src/components/app.js
@@ -13,23 +13,6 @@ import AboutUs from './about_us';
 const App = () => (
     <Router>
         <div>
-        {/* <div className="app">
-                <Splash/>
-            </div>
-            <ul>
-                <li>
-                    <Link to='/overview'>Overview</Link>>
-                </li>
-                <li>
-                    <Link to='/addnew'>Add New</Link>>
-                </li>
-                <li>
-                    <Link to='/login'>Login</Link>
-                </li>
-                <li>
-                    <Link to='/signup'>Sign Up</Link>
-                </li>
-            </ul> */}
             <Route path ="/add_new" component={AddNew}/>
             <Route exact path="/" component={Splash}/>
             <Route path ="/overview" component={Overview}/>
