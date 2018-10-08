da84da9d6dd4b196d630d2804a5d6bfceb2440ba
valor323 <caine101@mail.chapman.edu>
2018-10-08 11:04:38 -0700

    initial edit button

--- client/src/components/overview.js
+++ client/src/components/overview.js
@@ -5,22 +5,35 @@ import users from '../dummy_data/dummyList.js';
 import Accordion from './accordion_container';
 import AccordionItem from './accordion_item';
 import Footer from './footer';
-
+import response from '../dummy_data/dummyList.js';
+import EditRow from './editRow';
 class Overveiw extends Component{
     constructor(props){
         super(props);
     }
-    makeRow(){
-        const eachUser = [...users];
+    editRow (index){
+        const eachUser = [...response];
         const mapOfUsers = eachUser.map(item => item.receipts);
         const receiptUser = mapOfUsers[2];
-        const row = receiptUser.map(item => (
+        const storeName = receiptUser[index].storeName;
+        console.log(storeName);
+        let changeStoreName = prompt("Enter Store Name")
+        if(changeStoreName !== currentName){
+            currentName.text(changeStoreName);
+        }
+     }
+    makeRow(){
+        const eachUser = [...response];
+        const mapOfUsers = eachUser.map(item => item.receipts);
+        const receiptUser = mapOfUsers[2];
+        const row = receiptUser.map((item, index) => (
         <Accordion>
-            <div className="row">
+            <div className="row" attr={index}>
                 <h3 className="store_name">{item.storeName}</h3>
                 <br/>
-                <h4 className="date_of_purchase">{new Date().toLocaleDateString()}</h4>
+                <h4 className="date_of_purchase">{item.purchaseDate}</h4>
                 <h3 className="amount_of_purchase">${item.total}</h3>
+                <button onClick={() => this.editRow(index)} className="edit">Edit</button>
                     <AccordionItem className="panel">
                        <div className="panel_size">
                             <p className="catagory">Merchant name:</p>
@@ -28,7 +41,7 @@ class Overveiw extends Component{
                         </div>
                         <div className="panel_size">
                             <p className="catagory">Date of Purchase:</p>
-                            <h6 className="data">{new Date().toLocaleDateString()}</h6>
+                            <h6 className="data">{item.purchaseDate}</h6>
                         </div>
                         <div className="panel_size">
                             <p className="catagory">Total Amount:</p>
@@ -47,9 +60,9 @@ class Overveiw extends Component{
         </Accordion>
         ));
         return row
-     }
+        }
     render(){
-        const eachUser = [...users];
+        const eachUser = [...response];
         const mapOfUsers = eachUser.map(item => item.receipts);
         const receiptUser = mapOfUsers[2];
         const total = receiptUser.map(item => item.total);
