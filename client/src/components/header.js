84f304a06162cf65f76bb1b6f49d4009c8767448
Sarah Han <sarah.s.han@gmail.com>
2018-10-08 18:50:42 -0700

    hamburger wip

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import './header.css';
 import dots from '../assets/images/dots.png';
 import { Link } from 'react-router-dom';
+import Hamburger from 'react-sidebar';
 
 class Header extends Component {
     render() {
         return(
             <div className="headContainer header">
                 {this.props.title || ''}
+                <Hamburger
+                    sidebar={<b>Sidebar content</b>}
+                    open={this.state.sidebarOpen}
+                    onSetOpen={this.onSetSidebarOpen}
+
+                    
                 <Link to='/about_us' ><img className="dots" src={dots}/></Link>
             </div>
         )
