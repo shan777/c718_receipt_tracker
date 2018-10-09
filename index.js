5afbb0b81fa560a583e699913bc7c9665e7d759c
Kyle Pamintuan <kylepamintuan@gmail.com>
2018-10-08 17:57:00 -0700

    finished addReceipt backend validation.

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ server.post('/api/addTag', (request, response) => {
     };
 
     let userIdRedEx = /^[1-9][\d]*/;
-    let tagNameRegEx = /^[a-zA-Z \d-_]{2,}$/;
+    let tagNameRegex = /^[a-zA-Z \d-_]{2,}$/;
 
-    if (userIdRedEx.exec(userId) && tagNameRegEx.exec(tagName)){
+    if (userIdRedEx.exec(userId) && tagNameRegex.exec(tagName)){
         const connection = mysql.createConnection(sqrlDbCreds);
         connection.query("INSERT INTO tags (userId, tagName) VALUES (?, ?);",
                     [userId, tagName],
@@ -43,49 +43,64 @@ server.post('/api/addTag', (request, response) => {
 });
 
 server.post('/api/addReceipt', (request, response) => {
-    /*
-        INSERT INTO receipts (userId, storeName, total, tax, creditCardName, creditCardDigits, purchaseDate, category, comment, reimbursable) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-    */
-
-    const {userId, storeName, total, tax, creditCardName, creditCardDigits, purchaseDate, category, comment, reimbursable} = request.body;
+    
+    // Destructuring request object (w/ default values)
+    const {userId, storeName, total, tax=0, creditCardName=null, creditCardDigits=null, purchaseDate=getCurrentDate(), category=null, comment=null, reimbursable=0} = request.body;
     console.log("request data: ", request.body);
     
+    // Output message for front-end
     const output = {
         success: false
     };
 
-    let userIdRegEx = /^[1-9][\d]*$/; // will also use for: total, tax
-    let storeNameRegEx = /^[a-zA-Z \d-_]{2,}$/;
+    // Regex for all input data validation
+    let userIdRegex = /^[1-9][\d]*$/;
+    let storeNameRegex = /^[a-zA-Z \d-_]{2,}$/;
+    let totalRegex = /^[1-9][\d]*$/;
+    let taxRegex = /^[1-9][\d]*$/;
     let creditCardNameRegex = /^[a-zA-Z ]{2,}$/;
     let creditCardDigitsRegex = /^[\d]{4}$/;
     let purchaseDateRegex = /^\d{4}-{1}\d{2}-{1}\d{2}$/;
     let categoryRegex = /^[a-zA-Z]$/;
-    let commentRegex = /^[a-zA-Z\d .-*/$%!?()+=]$/;
+    let commentRegex = /^[a-zA-Z\d .\-*\/$%!?()+=]$/;
     let reimbursableRegex = /^[01]{1}$/;
 
-    /*
-    if (userIdRedEx.test(userId) && tagNameRegEx.test(storeName)){
-        const connection = mysql.createConnection(sqrlDbCreds);
-        connection.query("INSERT INTO tags (userId, tagName) VALUES (?, ?);",
-                    [userId, tagName],
-                    (error, result) => {
-                        console.log('query made');
-                        if (error){
-                            console.log('query error', error);
-                            response.send(output);
-                        }
-                        output.success = true;
-                        connection.end(() => {
-                            console.log('connection end');
-                        });                        
+    // Validating required input data
+    if(userIdRegex.test(userId) && storeNameRegex.test(storeName) && totalRegex.test(total) && purchaseDateRegex.test(purchaseDate)){
+        console.log('required input data valid');
+
+        // Validating optional input data
+        if((taxRegex.test(tax) || tax===0) && (creditCardNameRegex.test(creditCardName) || creditCardName===null) && (creditCardDigitsRegex.test(creditCardDigits) || creditCardDigits===null) 
+            && (categoryRegex.test(category) || category===null) && (commentRegex.test(comment) || comment===null) && (reimbursableRegex.test(reimbursable) || reimbursable===0)){
+            console.log('optional input data valid');
+
+            // Create connection to db
+            const connection = mysql.createConnection(sqrlDbCreds);
+
+            // Attempt SQL query
+            connection.query("INSERT INTO receipts (userId, storeName, total, tax, creditCardName, creditCardDigits, purchaseDate, category, comment, reimbursable) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
+                [userId, storeName, total, tax, creditCardName, creditCardDigits, purchaseDate, category, comment, reimbursable],
+                (error, result) => {
+                    console.log('query sent');
+
+                    if(error){
+                        console.log('query error', error);
                         response.send(output);
-                    });
+                    }
+                    output.success = true;
+
+                    // Close connection to db
+                    connection.end(() => { console.log('connection end'); });
+                    
+                    // Send status of query to front-end
+                    response.send(output);
+                });
+        }
     }
     else{
+        // Send status of query to front-end
         response.send(output);
     }
-    */
 });
 
 server.get('*', (request, response) => {
@@ -95,3 +110,9 @@ server.get('*', (request, response) => {
 server.listen(PORT, () => {
     console.log('server listening on port ' + PORT);
 });
+
+function getCurrentDate(){
+    let today = new Date();
+    let current_date = today.toISOString().slice(0,10);
+    return current_date;
+}
