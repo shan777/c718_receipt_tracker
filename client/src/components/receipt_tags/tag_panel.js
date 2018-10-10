f8d5a942de950d564cefafc51536f9fd292e2205
Sarah Han <sarah.s.han@gmail.com>
2018-10-10 16:47:42 -0700

    tag css updated on addnew page

--- client/src/components/receipt_tags/tag_panel.js
+++ client/src/components/receipt_tags/tag_panel.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReceiptTag from './receipt_tag';
 import './tag_panel.css';
 
-class TabPanel extends Component{
+class TagPanel extends Component{
     constructor(props){
         super(props);
         this.state = {
@@ -10,28 +10,32 @@ class TabPanel extends Component{
             addInputShowing: this.props.addCallback ? false : true
         }
     }
+
     renderAllTags(){
-        return this.props.tags.length>0 ? this.props.tags.map( (tagInfo,index)=> <ReceiptTag data={tagInfo} key={index}/>) : 'no tags available';
+        return this.props.tags.map((tagInfo, index)=> <ReceiptTag data={tagInfo} key={index}/>) ;
     }
-    handleTagAddButtonClick = ()=>{
+    
+    handleTagAddButtonClick = () => {
         this.setState({
             newTag: '',
             addInputShowing: !this.state.addInputShowing
         })
     }
-    handleTagAddComplete = (tagText) =>{
-        console.log('handle tag add complete ' + tagText)
+
+    handleTagAddComplete = (tagText) => {
         this.props.addCallback(tagText);
     }
+
     render(){
         return(
-            <div className="tab_panel">
-                { this.props.addCallback ? <div onClick={this.handleTagAddButtonClick} className="tagAddButton">{this.state.addInputShowing ? '-' : '+'}</div> : ''}
-                { this.state.addInputShowing ? <ReceiptTag data='' mode="add"  addHandler={this.handleTagAddComplete}/> : ''}
+            <div className="tag_panel">
+                {this.props.addCallback ? 
+                    <div onClick={this.handleTagAddButtonClick} className="tag_add_button">{this.state.addInputShowing ? '-' : '+'}</div> : ''}
+                {this.state.addInputShowing ? <ReceiptTag data='' mode="add" addHandler={this.handleTagAddComplete}/> : ''}
                 {this.renderAllTags()}
             </div>
         );
     }
 }
 
-export default TabPanel;
+export default TagPanel;
