d2cffe2a0b6b2ba9755343d371a81b49f2359412
valor323 <caine101@mail.chapman.edu>
2018-10-12 11:28:11 -0700

    overview working with server

--- client/src/components/overview.js
+++ client/src/components/overview.js
@@ -4,39 +4,52 @@ import './overview.css';
 import Accordion from './accordion_container';
 import AccordionItem from './accordion_item';
 import Footer from './footer';
-import response from '../dummy_data/dummyList.js';
-import {Link} from 'react-router-dom';
 import RenderTag from './receipt_tags/render_tag';
 import Modal from './modal';
+import axios from 'axios';
 
 class Overveiw extends Component{
     constructor(props){
         super(props);
 
         this.state = {
-            data: response,
-            currentDisplayedUserID: props.match.params.userID !==undefined ? props.match.params.userID : 2,
+            data: null,
+            currentDisplayedUserID: props.match.params.userID !==undefined ? props.match.params.userID : 3,
             isOpen: false,
-            activeButton: null
+            activeButton: null,
+            receiptId: null,
+            userId: null
         };
     }
     open = (index) => this.setState({isOpen: true, activeButton: index});
 
     close = () => this.setState({isOpen: false});
 
+    async componentDidMount(){
+        const login = await axios.post('/api/login', {userName: 'estherSuh', password: 'estherLfz123'})
+        this.setState({
+            userId: login.data.userId
+        });
+        console.log('User Id::', this.state.userId);
+        const axiosResponse = await axios.post('/api/getUserReceipts', {userId: this.state.userId})
+        
+        this.setState({
+            data: axiosResponse,
+        });
+        console.log(this.state.data);
+    }
+
     makeRow(){
-        const eachUser = [...this.state.data];
-        const mapOfUsers = eachUser.map(item => item.receipts);
-        const receiptUser = mapOfUsers[this.state.currentDisplayedUserID];
-        const row = receiptUser.map((item, index) => (
+        const currentUser = [...this.state.data.data.receipts];
+        const row = currentUser.map((item, index) => (
         <Accordion key={index}>
             <div className="row">
                 <div className="store_name">{item.storeName}</div>
                 <br/>
-                <div className="date_of_purchase">{item.purchaseDate}</div>
+                <div className="date_of_purchase">{item.purchaseDate.slice(0,10)}</div>
                 <div className="amount_of_purchase">${(item.total/100).toFixed(2)}</div>
                 <button className='editbtn' onClick={()=> this.open(index)}>
-                <i class="material-icons">edit</i></button>
+                <i className="material-icons">edit</i></button>
                     <AccordionItem className="panel">
                        <div className="panel_size">
                             <div className="catagory">Merchant name:</div>
@@ -44,7 +57,7 @@ class Overveiw extends Component{
                         </div>
                         <div className="panel_size">
                             <div className="catagory">Date of Purchase:</div>
-                            <div className="data">{item.purchaseDate}</div>
+                            <div className="data">{item.purchaseDate.slice(0,10)}</div>
                         </div>
                         <div className="panel_size">
                             <div className="catagory">Total Amount:</div>
@@ -52,13 +65,13 @@ class Overveiw extends Component{
                         </div>
                         <div className="panel_size">
                             <div className="catagory">Catagory:</div>
-                            <div className="data">{item.catagory}</div>
+                            <div className="data">{item.category}</div>
                         </div>
                         <div className="panel_size">
                             <div className="catagory">Note:</div>
                             <div className="data">{item.comment}</div>
                         </div>
-                        <RenderTag tags={item.tags} />
+                        {/* <RenderTag tags={item.tags} /> */}
                     </AccordionItem>
             </div>
         </Accordion>
@@ -66,10 +79,13 @@ class Overveiw extends Component{
         return row
         }
     render(){
-        const eachUser = [...response];
-        const mapOfUsers = eachUser.map(item => item.receipts);
-        const receiptUser = mapOfUsers[2];
-        const total = receiptUser.map(item => item.total);
+        if(!this.state.data){
+            return(
+                <h1>loading...</h1>
+            )
+        }
+        const currentUser = [...this.state.data.data.receipts];
+        const total = currentUser.map(item => item.total);
         const addTotal = () =>{
             let totalAmount = null;
             for(let i = 0; i< total.length; i++){
@@ -80,7 +96,7 @@ class Overveiw extends Component{
         if(this.state.isOpen){
             let id = 2;
             return (
-                <Modal row={this.state.activeButton} data={response} currentId={id} close={this.close}/>
+                <Modal row={this.state.activeButton} data={this.state.data} currentId={id} close={this.close}/>
             )
         }
         return (
@@ -89,7 +105,7 @@ class Overveiw extends Component{
                 <div className='overview_main_container'>
                     {this.makeRow()}
                     <div className="summary">
-                        <p className="number_of_receipts"><b>{receiptUser.length}</b> Receipts
+                        <p className="number_of_receipts"><b>{currentUser.length}</b> Receipts
                          -- <b>Total:</b> ${addTotal()}</p>
                     </div>
                 </div>
