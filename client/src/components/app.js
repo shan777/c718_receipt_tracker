32a43e9bca5e19d94dd96c62f8384495fd6a9d55
Esther <esther2180@gmail.com>
2018-10-13 17:03:58 -0700

    Initial About Page

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -8,6 +8,7 @@ import AddNew from './add_new';
 import Login from './login';
 import SignUp from './signup';
 import AboutUs from './about_us';
+import About from './about';
 
 const App = () => (
     <Router>
@@ -18,6 +19,7 @@ const App = () => (
             <Route path ="/login" component = {Login}/>
             <Route path="/signup" component ={SignUp}/>
             <Route path="/about_us" component={AboutUs}/>
+            <Route path="/about" component={About}/>
         </div>
     </Router>
 );
