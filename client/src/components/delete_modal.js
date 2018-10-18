721486bc2273465d388c928db24901de1dffb857
nebevets <37419695+nebevets@users.noreply.github.com>
2018-10-17 18:36:37 -0700

    Update delete_modal.js

--- client/src/components/delete_modal.js
+++ client/src/components/delete_modal.js
@@ -14,10 +14,10 @@ class DeleteModal extends Component {
                 <div className="basic_modal" onClick={this.close}>
                     <div className="basic_modal_content onClick={e => e.stopPropagation()}">
                         <div className="basic_modal_close" onClick={this.props.close}>X</div>'
-                    <div className="modal_container">
-                        <h4 className='sure'>Are you sure you want to delete this entry?</h4>
-                        <button className='surebtn' onClick={() => this.delete(this.props.receiptId)}>I'm Sure</button>
-                    </div>
+                        <div className="modal_container">
+                            <h4 className='sure'>Are you sure you want to delete this entry?</h4>
+                            <button className='surebtn' onClick={() => this.delete(this.props.receiptId)}>I'm Sure</button>
+                        </div>
                     </div>
                 </div>
             )
