d30ec8300259d0e0829f7790ab60337145229605
Sarah Han <sarah.s.han@gmail.com>
2018-10-30 16:35:08 -0700

    signup validation completed

--- client/src/components/signUp.js
+++ client/src/components/signUp.js
@@ -18,7 +18,7 @@
         }
     }
   
-    handleValidation(e) {
+    async handleSubmit(e) {
         e.preventDefault();
 
         let fields = this.state.fields;
@@ -33,8 +33,8 @@
   
         if(typeof fields["username"] !== "undefined"){
             if(!fields["username"].match(/^(?=.{4,15}$)(?!.*[_]{2})^[a-zA-Z]\w+(?<![_])$/i)){
-            formIsValid = false;
-            errors["username"] = "Invalid Username";
+                formIsValid = false;
+                errors["username"] = "Invalid Username";
             }      	
         }
   
@@ -46,8 +46,8 @@
     
         if(typeof fields["firstName"] !== "undefined"){
             if(!fields["firstName"].match(/[a-zA-Z_.-]{2,}/)){
-            formIsValid = false;
-            errors["firstName"] = "Invalid First Name";
+                formIsValid = false;
+                errors["firstName"] = "Invalid First Name";
             }      	
         } 
   
@@ -63,11 +63,61 @@
             errors["lastName"] = "Invalid Last Name";
             }      	
         } 
+
+        // Password validation
+        if(!fields["password"]){
+            formIsValid = false;
+            errors["password"] = "Password cannot be empty";
+        }
+    
+        if(typeof fields["password"] !== "undefined"){
+            if(!fields["password"].match(/^(?=[a-zA-Z])(?=.{8,32}$)(?=.*[A-Z])(?=.*\d)(?=.*[a-z]).*$/)){
+            formIsValid = false;
+            errors["password"] = "Invalid Password";
+            }      	
+        } 
   
+        // Confirm Password validation
+        if(!fields["confirmPassword"]){
+            formIsValid = false;
+            errors["confirmPassword"] = "Confirm Password cannot be empty";
+        }
+    
+        if(typeof fields["confirmPassword"] !== "undefined"){
+            if(fields["password"] !== fields["confirmPassword"]){
+            formIsValid = false;
+            errors["confirmPassword"] = "Does not match";
+            }      	
+        } 
+
         this.setState({errors: errors});
-        return formIsValid;
+
+        if (formIsValid) {
+            const {username, firstName, lastName, password} = this.state.fields;
+
+            try {
+                await axios.post('/api/manageUsers/signUp',{
+                    userName: username,
+                    password: password,
+                    firstName: firstName,
+                    lastName: lastName,
+                });
+
+                this.props.history.push('/overview');
+                
+            } catch(err) {
+                if(err.response.data.error.code == "ER_DUP_ENTRY") {
+                    formIsValid = false;
+                    errors["username"] = `This username exists already.\nTry a different username.`;
+                    this.setState({errors: errors});
+
+                    return formIsValid;
+                }
+            }
+            
+        } else
+            return formIsValid;
     }
-  
    
   
     handleChange(field, e){    		
@@ -79,41 +129,47 @@
   
     render(){
         return (
-    
             <div className="signup_container">
-                <div className="sign_up_main_container">
-                    <img className="signup_logo" src={squirrel}></img>
-                    <div className="title">Create an account to start</div>
- 
-                            	
-                    <form name="contactform" className="contactform" onSubmit= {this.handleValidation.bind(this)}>
-                        {/* <div className="entry_container">    */}
-                            <fieldset>
-                                <input className="username_input" ref="username" type="text" size="20" placeholder="Username" onChange={this.handleChange.bind(this, "username")} value={this.state.fields["username"]}/>
-                                <div className="error_p">&nbsp;&nbsp;&nbsp;&nbsp;<i className="material-icons">info</i>&nbsp;<b>Username Guidelines:</b>
-                                    <ul>
-                                        <li>Only letters (a-z), numbers (0-9), and underscores(_)</li>
-                                        <li>Must start and end with a letter</li>
-                                        <li>Between 4-15 characters long</li>
-                                    </ul>
-                                </div>
-                                <span className="error">{this.state.errors["username"]}</span>
-                                <br/>
-                                <input ref="firstName" type="text" size="30" placeholder="First Name" onChange={this.handleChange.bind(this, "firstName")} value={this.state.fields["firstName"]}/>
-                                <span className="error">{this.state.errors["firstName"]}</span>
-                                <br/>
-                                <input ref="lastName" type="text" size="30" placeholder="Last Name" onChange={this.handleChange.bind(this, "lastName")} value={this.state.fields["lastName"]}/>
-                                <span className="error">{this.state.errors["lastName"]}</span>
-                                <br/>
-                            </fieldset>
-                         {/* </div> */}
-            
-                        <fieldset>
-                                <button className="letsgo" id="submit" value="Submit">Let's go</button>
-                        </fieldset>
-                    </form>
-
-                </div>
+                <img className="signup_logo" src={squirrel}></img>
+                            
+                <form name="contactform" className="contactform" onSubmit= {this.handleSubmit.bind(this)}>
+                    <fieldset>
+                        <input className="username_input" ref="username" type="text" size="20" placeholder="Username" onChange={this.handleChange.bind(this, "username")} value={this.state.fields["username"]}/>
+                        <div className="guideline">&nbsp;&nbsp;&nbsp;<i className="material-icons">info</i>&nbsp;<b>Username Guidelines:</b>
+                            <ul>
+                                <li>Only letters (a-z), numbers (0-9), and underscores(_)</li>
+                                <li>Must start and end with a letter</li>
+                                <li>Between 4-15 characters long</li>
+                            </ul>
+                        </div>
+                        <span className="error">{this.state.errors["username"]}</span>
+                        <br/>
+                        <input ref="firstName" type="text" size="20" placeholder="First Name" onChange={this.handleChange.bind(this, "firstName")} value={this.state.fields["firstName"]}/>
+                        <span className="error">{this.state.errors["firstName"]}</span>
+                        <br/>
+                        <input ref="lastName" type="text" size="20" placeholder="Last Name" onChange={this.handleChange.bind(this, "lastName")} value={this.state.fields["lastName"]}/>
+                        <span className="error">{this.state.errors["lastName"]}</span>
+                        <br/>
+                        <input className="password_input" ref="password" type="password" size="20" placeholder="Password" onChange={this.handleChange.bind(this, "password")} value={this.state.fields["password"]}/>
+                        <div className="guideline">&nbsp;&nbsp;&nbsp;<i className="material-icons">info</i>&nbsp;<b>Password Guidelines:</b>
+                            <ul>
+                                <li>Must contain at least 1 uppercase AND 1 lowercase AND 1 number</li>
+                                <li>Must start with a letter</li>
+                                <li>No special characters</li>
+                                <li>Between 8-32 characters long</li>
+                            </ul>
+                        </div>
+                        <span className="error">{this.state.errors["password"]}</span>
+                        <br/>
+                        <input ref="confirmPassword" type="password" size="20" placeholder="Confirm Password" onChange={this.handleChange.bind(this, "confirmPassword")} value={this.state.fields["confirmPassword"]}/>
+                        <span className="error">{this.state.errors["confirmPassword"]}</span>
+                        <br/>
+                    </fieldset>
+        
+                    <fieldset>
+                            <button className="signup_button" id="submit" value="Submit">Sign up</button>
+                    </fieldset>
+                </form>
             </div>
         );
     }
