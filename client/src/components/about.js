611653c11d8515e5ba319e15e8421808c64bda9a
Sarah Han <sarah.s.han@gmail.com>
2018-11-06 01:52:28 -0800

    about app page updated w/ description of what this app does and etc and some css was updated as well

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './header';
 import './about.css';
+import Sqr from '../assets/images/tiny_squirrel.png';
 import HTML from '../assets/images/html.png';
 import CSS from '../assets/images/css.png';
 import JS from '../assets/images/js.png';
@@ -14,27 +15,42 @@ class About extends Component {
     render() {
         return (
             <div>
-                <Header title="TECH USED" />
+                <Header title="ABOUT" />
                 <div className="about_container">
+                    <p><img className="sqr" src={Sqr} alt="sqr logo"/><b>SquirrelReceipts</b> App was created to help people to manage receipts 
+                    by receiving them digitally to this app instead of paper receipts.
+                    <br />Our main goal is to eliminate all paper receipts and receive / access all your receipts in this app. 
+                    You are also helping the environment by reducing waste and killing less trees! :D
+                    </p>
                     <div className="tech_container">
-                        <p>Lists of technologies</p>
+                        <div className="tech_title"><b><u>TECHNOLOGIES USED</u></b></div><br/>
                         <div className="app_container_top">
-                            <img className="html_logo" src={HTML}/>
-                            <img className="css_logo" src={CSS}/>
-                        </div>
-                        <div className="app_container_center">
-                            <img className="js_logo" src={JS}/>
-                            <img className="react_logo" src={ReactJS}/>
+                            <table>
+                                <tbody>
+                                    <tr><th colSpan="4">Front-End</th></tr>
+                                    <tr>
+                                        <td><img className="html_logo imgs" src={HTML}/></td>
+                                        <td><img className="css_logo imgs" src={CSS}/></td>
+                                        <td><img className="js_logo imgs" src={JS}/></td>
+                                        <td><img className="react_logo imgs" src={ReactJS}/></td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                         <div className="app_container_bottom">
-                            <img className="mySql_logo" src={MySql}/>
-                            <img className="node_logo" src={Node}/>
-                            <Link to="/overview"><p className="domain">www.squirrelreceipts.com</p></Link>
+                            <table>
+                                <tbody>
+                                    <tr><th colSpan="4">Back-End</th></tr>
+                                    <tr>
+                                        <td colSpan="2"><img className="mySql_logo imgs" src={MySql}/></td>
+                                        <td colSpan="2"><img className="node_logo imgs" src={Node}/></td>
+                                    </tr>
+                                </tbody>
+                            </table>    
                         </div>
+                        <Link to="/overview"><div className="domain"><b><i>www.squirrelreceipts.com</i></b></div></Link>
                     </div>
                 </div>
-                
-                
             </div>
         )
     }
